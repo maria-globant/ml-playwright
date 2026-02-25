f6ad890bbf65931f12259c7e5c2165dacea676c1
Maria Globant <tuemail@empresa.com>
2026-02-25 16:58:17 -0300

    eShop-page---1

---
+++ tests/eShop.spec.js
@@ -0,0 +1,21 @@
+import { expect, test } from "@playwright/test";
+
+test.only("Ecommerce page", async ({ browser }) => 
+{
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+    const userName = page.locator("#username");
+    const password = page.locator("#password");
+    const signInBtn = page.locator("#signInBtn")
+    await userName.fill("rahulshettyacademy");
+    await password.fill("Learning@830$3mK2");  
+    //await signInBtn.click();
+
+
+    await Promise.all([page.waitForURL('**/angularpractice/shop'), page.click('#signInBtn')]);
+    console.log ("Titulo : ",await page.title());
+ 
+});
