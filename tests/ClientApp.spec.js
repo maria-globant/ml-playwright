4ebe11ebc69791c866ec53c31364b5f21add5c3f
Maria Globant <tuemail@empresa.com>
2026-03-03 14:49:41 -0300

    Lesson 18: Playwright Test - Assertions and Test Hooks

---
+++ tests/ClientApp.spec.js
@@ -0,0 +1,23 @@
+// corrida para ver las pantallas: npx playwright test --headed
+
+import { expect, test } from "@playwright/test";
+
+
+test.only("Successfully Login Password", async ({ page }) => 
+{
+    await page.goto("https://rahulshettyacademy.com/client/");
+    const userName = await page.locator("#userEmail").fill("anshika@gmail.com");
+    const password = await page.locator("#userPassword").fill("Iamking@000");
+    //const signInBtn = page.locator("[value='Login']").click();
+    const signInBtn = page.locator("#login")
+    await signInBtn.click();
+
+    //await page.waitForLoadState("networkidle");
+
+   await page.locator(".card-body b").first().waitFor();    
+
+    const tiles = await page.locator(".card-body b").allTextContents();
+
+    console.log(tiles);
+
+});
