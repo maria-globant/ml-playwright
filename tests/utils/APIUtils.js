b2637e2ca4f3b764e76cf14690f64af21ca9826e
Maria Laura Estefania <maria.estefania@globant.com>
2026-06-19 17:37:21 -0300

    Lesson 59 - Web API Testing with Playwright

--- tests/utils/APIUtils.js
+++ tests/utils/APIUtils.js
@@ -1,7 +1,20 @@
+import { expect, test, request } from "@playwright/test";
+
 class APIUtils {
 
     constructor(apiContext) {
         this.apiContext = apiContext;
+        this.orderPayload = {
+            orders: [
+                {
+                    country: "India",
+                    productOrderedId: "6960eac0c941646b7a8b3e68"
+                }
+            ]
+        };
+        this.loginPayload = { 
+            userEmail: "mlestefania@hotmail.com", 
+            userPassword: "Automation$385" };
     }
 
     async getToken() {
@@ -9,19 +22,19 @@ class APIUtils {
         // Login API para obtener el token
 
         const loginResponse = await this.apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login", {
-            data: loginPayload
+            data: this.loginPayload
         });
         expect(loginResponse.ok()).toBeTruthy();
         const loginResponseJson = await loginResponse.json();
-        token = loginResponseJson.token;
-        console.log("Token:", token);
-        return token;
+        console.log("Login Response Token:", loginResponseJson.token);
+    
+        return loginResponseJson.token;
     }
 
     async createOrder() {
 
         const orderResponse = await this.apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order", {
-            data: orderPayload,
+            data: this.orderPayload,
             headers: {
                 'Authorization': await this.getToken(),
                 'Content-Type': 'application/json'
@@ -29,9 +42,11 @@ class APIUtils {
         });
         const orderResponseJson = await orderResponse.json();
         console.log("Order Response:", orderResponseJson);
-        orderID = orderResponseJson.orders[0];
+        const orderID = orderResponseJson.orders[0];
         console.log("Order ID:", orderID);
         return orderID;
     }
 
 }
+
+export default APIUtils;
