c2e171926d92025faecad6e62c977c5be0236937
Maria Laura Estefania <maria.estefania@globant.com>
2026-06-24 16:36:17 -0300

    Lesson 59: Refactor APIUtils to return both token and orderId, and update test assertions

--- tests/WebAPIPart1.spec.js
+++ tests/WebAPIPart1.spec.js
@@ -7,19 +7,21 @@ import APIUtils from "./utils/APIUtils.js";
 // se pueden seleccionar locators, diferentes a los que vienen del flujo,  
 // para hacer la corrida con --ui, te permite seleccionar que queres corrar y las pantallas q va corriendo
 
-//const loginPayload = { userEmail: "mlestefania@hotmail.com", userPassword: "Automation$385" };
-//const orderPayload = { orders: [{ country: "India", productOrderedId: "6960eac0c941646b7a8b3e68" }] };
+const loginPayload = { userEmail: "mlestefania@hotmail.com", userPassword: "Automation$385" };
+const orderPayload = { orders: [{ country: "India", productOrderedId: "6960eac0c941646b7a8b3e68" }] };
 
 // https://rahulshettyacademy.com/api/ecom/order/create-order
 
 //let token;
 //let orderID;
-let apiContext;
+//let apiContext;
+let response;
 
 test.beforeAll(async () => {
 
-    apiContext = await request.newContext();
-
+    const apiContext = await request.newContext();
+    const apiUtils = new APIUtils(apiContext, loginPayload);
+    response = await apiUtils.createOrder(orderPayload);
 });
 
 test.beforeEach(() => {
@@ -29,19 +31,22 @@ test.beforeEach(() => {
 
 test.only("Web Api validations", async ({ page }) => {
 
-    const ApiUtils = new APIUtils(apiContext);
-    const orderID = await ApiUtils.createOrder();
-    const email = ApiUtils.loginPayload.userEmail;
-    const country = ApiUtils.orderPayload.orders[0].country;
+    //const ApiUtils = new APIUtils(apiContext, loginPayload);
+    //const orderID = await ApiUtils.createOrder(orderPayload);
 
-    console.log("Order ID from API:", orderID);
-    const token = await ApiUtils.getToken();
-    console.log("Token:", token);
+    //const email = apiUtils.loginPayload.userEmail;
+    //const country = apiUtils.orderPayload.orders[0].country;
 
+    //console.log("Order ID from API:", response.orderId);
+    //const token = await ApiUtils.getToken();
+    //console.log("Token:", token);
+
+    const email = loginPayload.userEmail;
+    const country = orderPayload.orders[0].country;
 
     await page.addInitScript(value => {
         window.localStorage.setItem("token", value);
-    }, token);
+    }, response.token);
     //const email = loginPayload.userEmail;
 
     await page.goto("https://rahulshettyacademy.com/client/", { timeout: 60000 });
@@ -58,10 +63,10 @@ test.only("Web Api validations", async ({ page }) => {
 
     for (let i = 0; i < orderCount; i++) {
         const rowText = await orderTable.nth(i).locator("th").textContent();
-        if (rowText.includes(orderID)) {
+        if (response.orderId.includes(rowText)) {
             orderFound = true;
             await orderTable.nth(i).locator("button").first().click();
-            console.log("Orden encontrada en el historial:", orderID);
+            console.log("Orden encontrada en el historial:", response.orderId);
             break;
         }
     }
@@ -69,10 +74,11 @@ test.only("Web Api validations", async ({ page }) => {
     const orderIdDetails = await page.locator(".col-text").textContent();
 
     await page.pause();
-    expect(orderID.includes(orderIdDetails)).toBeTruthy();
+    expect(response.orderId.includes(orderIdDetails)).toBeTruthy();
 
 
     await page.locator(".email-wrapper").waitFor({ state: "visible" });
+
 
     const billingEmail = await page.locator('div.address', { hasText: 'Billing Address' }).locator('p.text').nth(0).textContent();
     const billingEmailCountryFull = await page.locator('div.address', { hasText: 'Billing Address' }).locator('p.text').nth(1).textContent();
@@ -83,11 +89,11 @@ test.only("Web Api validations", async ({ page }) => {
     const billingEmailCountry = billingEmailCountryFull.split(" - ")[1].trim(); // "India"
     const deliveryCountry = deliveryCountryFull.split(" - ")[1].trim(); // "India"
 
-
     expect(billingEmail.trim()).toBe(email);
     expect(deliveryEmail.trim()).toBe(email);
     expect(billingEmailCountry.trim()).toBe(country);
     expect(deliveryCountry.trim()).toBe(country);
+
 
     //// Validar que el producto en el detalle de la orden sea el mismo que se ordenó por API
 
